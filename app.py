0db4de735bafc7ec62d4f88b94dd7b77b9892cf4
bgibso30 <128432574+bgibso30@users.noreply.github.com>
2023-03-27 15:16:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import numpy as np
 
 
 
-df = pd.read_csv('customer_shopping_data.csv')
-
-
+df = pd.read_csv(r'C:/Users/briaa/Documents/streamlit/customer_shopping_data.csv')
 
 st.sidebar.header("Pick two variables for your bar chart")
 x_val = st.sidebar.selectbox("Pick your x-axis",df.select_dtypes(include=object).columns.tolist())
